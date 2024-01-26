20db27c297c1bc2721017f71dcacf252e7ac20ff
estelledumont84 <estelle.dumont84@gmail.com>
2024-01-26 09:05:53 +0100

    delete users

--- src/app.js
+++ src/app.js
@@ -20,8 +20,7 @@ app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 app.post("/api/movies", movieControllers.postMovie);
 app.put("/api/movies/:id", movieControllers.updateMovie);
-
-
+app.delete("/api/movies/:id", movieControllers.deleteMovie);
 
 const userControllers = require("./controllers/userControllers");
 
@@ -29,7 +28,7 @@ app.get("/api/users", userControllers.getUsers);
 app.get("/api/users/:id", userControllers.getUserById);
 app.post("/api/users", userControllers.postUsers);
 app.put("/api/users/:id", userControllers.updateUser);
-
+app.delete("/api/users/:id", userControllers.deleteUser);
 
 
 
--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -66,7 +66,24 @@ const updateUser = (req, res) => {
       res.sendStatus(500);
     });
 
+};const deleteUser = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("delete from users where id = ?", [id])
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
 };
+
 
 
 module.exports = {
@@ -74,4 +91,5 @@ module.exports = {
   getUserById,
   postUsers,
   updateUser,
+  deleteUser,
 };
